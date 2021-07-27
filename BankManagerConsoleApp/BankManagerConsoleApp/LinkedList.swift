c2f56258956d6612c7c3e64c0a36bb2ced40f338
yoonju lee <rsyjlee@gmail.com>
2021-07-27 20:57:07 +0900

    머지 문제 해결

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,34 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by Do Yi Lee on 2021/07/27.
+//
+
+import Foundation
+
+class LinkedList<Value> {
+    private var head: Node<Value>?
+    private var tail: Node<Value>?
+    
+    func isEmpty() -> Bool {
+        return head == nil
+    }
+    
+    func append(_ value: Value) {
+        guard !isEmpty() && tail != nil else {
+            head = Node(value: value)
+            tail = head
+            return
+        }
+        
+        tail?.next = Node(value: value)
+        tail = tail?.next
+    }
+    
+    func remove(_ value: Value) -> Value? {
+        defer { head = head?.next }
+        
+        return head?.value ?? nil
+    }
+}
