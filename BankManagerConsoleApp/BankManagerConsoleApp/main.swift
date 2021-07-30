aacd617acec993b63e8d23e959a6a2642388adb4
Ldoy <leedoy7989@gmail.com>
2021-07-30 19:53:34 +0900

    feat: step3  기능 대략적으로 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -8,11 +8,20 @@ import Foundation
 
 struct Bank {
     var totalBanker: Int = 1
+    let totalCustomerNumber: Int
+    
+    init() {
+        let range = (10...30)
+        let totalNumber = Int.random(in: range)
+        self.totalCustomerNumber = totalNumber
+    }
+    
 }
 
 extension Bank {
     mutating func makingWaitingLine(from totalCustomerNumber: Int) -> Queue<Int> {
         var waitingLine = Queue<Int>()
+        
         for i in 1...totalCustomerNumber {
             waitingLine.enqueue(i)
         }
@@ -27,56 +36,138 @@ extension Bank {
     }
 }
 
+enum Task: String, CaseIterable {
+    case 대출1, 대출2, 예금
+}
+
 struct Customer {
-    var number: Int
+    let task: String
+    let tagNumber: Int
     
-    init() {
-        let range = (10...30)
-        let totalNumber = Int.random(in: range)
-        self.number = totalNumber
+    init(tagNumber: Int) {
+        let randomTask = Task.allCases.randomElement()?.rawValue
+        self.task = randomTask ?? " "
+        self.tagNumber = tagNumber
     }
 }
+
 
 struct Banker {
     var capacityCustomerNumber: Int = 1
 }
 
 extension Banker {
-    mutating func takeTask(watingLine: inout Queue<Int>) {
+    mutating func startTask(watingLine: inout Queue<Int>) {
         let customer = watingLine.peek()
         print("\(String(describing: customer))번 고객 업무가 시작되었습니다.")
+        
+    }
+    
+    mutating func endTask(watingLine: inout Queue<Int>) {
         let work = watingLine.dequeue()
         print("\(String(describing: work))번 고객 업무가 끝났습니다.)")
     }
 }
 
 
-
-func start() {
+func main3() {
+    // 고객의 태그와 헤드의 넘버가 값으면 고객의 테스크를 출려
+    // 고객의 테스크가 대출이면 특정 쓰레드?
+ 
+    // 시작은 메인큐에서, 끝내는 건 async
     var bank = Bank()
-    var banker = Banker()
-    let totalCustomer = Customer().number
-    var waitingLine = bank.makingWaitingLine(from: totalCustomer)
-    
-    for _ in 1...totalCustomer {
-    banker.takeTask(watingLine: &waitingLine)
+    var total = bank.totalCustomerNumber
+    var a = bank.makingWaitingLine(from: total)
+
+    for i in 1...total {
+        let customer = Customer(tagNumber: i)
+        switch customer.task {
+        case "대출1":
+            print("\(customer.tagNumber) 대출업무 시작")
+            a.dequeue()
+            DispatchQueue.global().async {
+                print("\(customer.tagNumber)대출 업무끝남")
+            }
+        case "대출2":
+            print("\(customer.tagNumber)번 대출업무 시작")
+            a.dequeue()
+            DispatchQueue.global().async {
+                print("\(customer.tagNumber)대출 업무끝남")
+            }
+        default:
+            a.dequeue()
+            print("\(customer.tagNumber) 예금업무 시작")
+            DispatchQueue.global().async {
+                print("\(customer.tagNumber)예금 업무끝남")
+            }
+        }
     }
     
-    bank.notifyClosing(totalCustomer: totalCustomer)
-}
-
-func end() {
-    return
-}
-
-func main() {
-    let input = readLine()
+    bank.notifyClosing(totalCustomer: total)
     
-    if input == "1" {
-        start()
-        
-    }
-    end()
 }
 
-main()
+
+
+main3()
+//func start() {
+//    var bank = Bank()
+//    var banker1 = Banker()
+//    var banker2 = Banker()
+////    var banker3 = Banker()
+//    let totalCustomer = bank.totalCustomerNumber
+//    //    let customerTask = customer.task
+//    var waitingLine = bank.makingWaitingLine(from: totalCustomer)
+//
+//
+//}
+
+//func end() {
+//    return
+//}
+//
+//func main() {
+//    let input = readLine()
+//
+//    if input == "1" {
+//        start()
+//
+//    }
+//    end()
+//}
+//
+//main()
+//
+
+
+
+//func start() {
+//    var bank = Bank()
+//    var banker = Banker()
+//    let totalCustomer = bank.totalCustomerNumber
+////    let customerTask = customer.task
+//
+//    var waitingLine = bank.makingWaitingLine(from: totalCustomer)
+//
+//    for _ in 1...totalCustomer {
+//    banker.takeTask(watingLine: &waitingLine)
+//    }
+//
+//    bank.notifyClosing(totalCustomer: totalCustomer)
+//}
+//
+//func end() {
+//    return
+//}
+//
+//func main() {
+//    let input = readLine()
+//
+//    if input == "1" {
+//        start()
+//
+//    }
+//    end()
+//}
+//
+//main()
